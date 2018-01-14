c81e43a5afeadf02858299564b4b1ab520cda9bd
Zella Urquhart <zella.urquhart@gmail.com>
2018-01-14 14:18:53 -0800

    Initial Commit

---
+++ Exercise2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exercise2
+//
+//  Created by Zella Urquhart on 1/14/18.
+//  Copyright © 2018 zellau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
